f7bd4980bd5afc26147f901b6d00126c9a307731
Bảo Đỗ Chí <cbao97@gmail,com>
2020-09-09 01:09:16 +0700

    Updates

---
+++ precache-manifest.29f43ba3d1c2352542a51be4572c9881.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28ef2bc680c289c8258188bf672612fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87a8add48f20bdfabdb6",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87a8add48f20bdfabdb6",
+    "url": "/static/js/main.2e91ef4d.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
