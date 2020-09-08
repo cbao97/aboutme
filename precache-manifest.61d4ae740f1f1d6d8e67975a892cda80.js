4e70cfc4f95616353c659fbe30e7d96166d9b69b
Bảo Đỗ Chí <cbao97@gmail,com>
2020-09-08 21:42:52 +0700

    Updates

---
+++ precache-manifest.61d4ae740f1f1d6d8e67975a892cda80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d035fdda8232a76f2487bda9f230cee",
+    "url": "/aboutme/index.html"
+  },
+  {
+    "revision": "c1ec16a85442fea26855",
+    "url": "/aboutme/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/aboutme/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/aboutme/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ec16a85442fea26855",
+    "url": "/aboutme/static/js/main.6786491a.chunk.js"
+  },
+  {
+    "revision": "1df459222ebff16b3dfe",
+    "url": "/aboutme/static/js/runtime-main.45a248eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aboutme/static/media/logo.5d5d9eef.svg"
+  }
+]);
